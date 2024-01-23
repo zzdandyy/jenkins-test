e7eadb3534434741c5e6eb0955cf9edc41a99b68
yuyuyu <1012061740@qq.com>
2024-01-23 18:06:40 +0800

    test

--- src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.jenkinstest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class JenkinsTestApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(20, 20);
     }
 
 }
--- src/test/java/com/example/jenkinstest/service/HelloServiceTest.java
+++ src/test/java/com/example/jenkinstest/service/HelloServiceTest.java
@@ -2,12 +2,22 @@ package com.example.jenkinstest.service;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
 public class HelloServiceTest {
+
+    @InjectMocks
+    private HelloService helloService;
 
     @Test
     public void testSayHello() {
-        HelloService helloService = new HelloService();
         helloService.hello();
+        assertEquals(20, 20);
     }
 }

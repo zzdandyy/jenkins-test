d857bfdfb7b63a57a33f609163cb0b7452c8290f
yuyuyu <1012061740@qq.com>
2024-01-23 19:46:08 +0800

    test

--- src/test/java/com/example/jenkinstest/service/HelloServiceTest.java
+++ src/test/java/com/example/jenkinstest/service/HelloServiceTest.java
@@ -3,16 +3,17 @@ package com.example.jenkinstest.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest()
 public class HelloServiceTest {
 
-    @InjectMocks
+    @Autowired
     private HelloService helloService;
 
     @Test

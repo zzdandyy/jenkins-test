dddd9f6329b4c992318bb8ea64e4881e9629f95b
yuyuyu <1012061740@qq.com>
2024-01-26 19:51:37 +0800

    test3

--- src/main/java/com/example/jenkinstest/service/HiService.java
+++ src/main/java/com/example/jenkinstest/service/HiService.java
@@ -48,4 +48,12 @@ public class HiService {
             return 1;
         }
     }
+
+    public int hi6(int a) {
+        if (a == 1) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }

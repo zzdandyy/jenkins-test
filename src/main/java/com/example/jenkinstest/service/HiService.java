300f01cda66dea70389fd08900b01570f3eae0bc
yuyuyu <1012061740@qq.com>
2024-01-29 10:21:38 +0800

    commit2-test

--- src/main/java/com/example/jenkinstest/service/HiService.java
+++ src/main/java/com/example/jenkinstest/service/HiService.java
@@ -56,4 +56,20 @@ public class HiService {
             return 1;
         }
     }
+
+    public int hi7(int a) {
+        if (a == 1) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public int hi8(int a) {
+        if (a == 1) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }

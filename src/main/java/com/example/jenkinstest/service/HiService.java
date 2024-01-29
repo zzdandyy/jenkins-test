36bcd016480376880774c647ee8008ab111199b9
yuyuyu <1012061740@qq.com>
2024-01-29 10:15:21 +0800

    test5 d,as,asd,sda,a

--- src/main/java/com/example/jenkinstest/service/HiService.java
+++ src/main/java/com/example/jenkinstest/service/HiService.java
@@ -56,20 +56,4 @@ public class HiService {
             return 1;
         }
     }
-
-    public int hi7(int a) {
-        if (a == 1) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-    public int hi8(int a) {
-        if (a == 1) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
 }
